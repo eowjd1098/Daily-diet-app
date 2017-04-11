a3a47c8a0b763b12f9dd6ccac4ba1657cf69f2d8
jung dae jung <rok1098@naver.com>
2017-04-11 16:56:12 +0900

    test

---
+++ ImgSaveSystem/ImgSaveSystem/Img.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ImgSaveSystem
+{
+    [Serializable]
+    class Img
+    {
+        public Img()
+        {
+            ImgeUrl = string.Empty;
+        }
+
+        public Img(XmlNode imgeurl)
+        {
+            string img = imgeurl.SelectSingleNode("link").InnerText;
+            ImgeUrl = img;
+        }
+
+
+        public string ImgeUrl
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
