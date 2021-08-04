01c10ec3fb61e14a097c883d772fa19da9e6346c
jung dae jung <rok1098@naver.com>
2021-08-04 16:48:18 +0900

    Delete Server directory

--- Server/Rest_api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-# Create your models here.
-
-class Member(models.Model):
-    member_id = models.CharField(max_length=100)
-    member_pw = models.CharField(max_length=100)
-    member_name = models.CharField(max_length=100)
-    member_age = models.CharField(max_length=20)
-    member_gender = models.CharField(max_length=20)
